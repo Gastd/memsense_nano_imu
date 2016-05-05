aa24ff8a2ea785d465be61bce88544239b907667
Gastd <gabriel.araujo.5000@gmail.com>
2016-05-05 18:33:32 -0300

    Add George's code

---
+++ include/imu.h
@@ -0,0 +1,103 @@
+/**
+ * @file      imu.h
+ * @author    George Andrew Brindeiro
+ * @date      03/12/2011
+ *
+ * @attention Copyright (C) 2011
+ * @attention Laboratório de Automação e Robótica (LARA)
+ * @attention Departamento de Engenharia Elétrica (ENE)
+ * @attention Universidade de Brasília (UnB)
+ */
+
+/* Definitions */
+
+// Size of IMU data packets
+#define IMU_PACKET_SIZE 38
+
+// Digital sensitivy for each of the sensors
+#define DS_GYR			1.3733E-2
+#define DS_ACC			9.1553E-5
+#define DS_MAG			8.6975E-5
+
+// Thermometer in the gyros also have an offset
+#define DS_TMP			1.8165E-2
+#define OS_TMP			25
+
+// Time scale for 16-bit time in seconds/count
+#define SC_TMR			2.1701E-6
+
+// Aux print-outs
+#define FATAL(x)		printf("\033[31m\033[1mFATAL:\033[0;0m\t%s\n",x)
+#define ERROR(x)		printf("\033[33m\033[1mERROR:\033[0;0m\t%s\n",x)
+#define WARN(x)			printf("\033[32m\033[1mWARN:\033[0;0m\t%s\n",x)
+#define INFO(x)			printf("\033[0;0m\033[1mINFO:\033[0;0m\t%s\n",x)
+#define DEBUG(x)		printf("\033[36m\033[1mDEBUG:\033[0;0m\t%s\n",x)
+#define VERBOSE(x)		printf("\033[34m\033[1mVERBOSE:\033[0;0m\t%s\n",x)
+
+#define ERROR2(x,y)		printf("\033[33m\033[1mERROR:\033[0;0m\t%s (%d)\n",x,y)
+
+/* IMU data struct */
+typedef struct imu
+{
+   double t;
+   double g[3];
+   double a[3];
+   double m[3];
+   double tmp[3];
+} imu_t;
+
+/* Function Prototypes */
+
+int imu_init(char* serial_port);
+int imu_get_data(imu_t* data);
+void imu_decode(imu_t* data, unsigned char imu_data[]);
+int imu_close();
+
+int imu_checksum(unsigned char chksum, unsigned char imu_data[]);
+
+void imu_print_raw(unsigned char imu_data[]);
+void imu_print_formatted(imu_t* data);
+
+/** ### MEMSense NanoIMU Specifications ###
+ *
+ *  Serial port:
+ *
+ *  (#)	Speed					115200
+ *  (#) Parity					None
+ * 	(#)	Bits					8
+ * 	(#) Stopbits				1
+ * 	(#) Flow control			None
+ *
+ *  Gyrometer:
+ *
+ * 	(#) Dynamic Range 			+-300 º/s
+ * 	(#) Offset					+-1.5 º/s
+ * 	(#) Cross-axis sensitivity	+-1 %
+ * 	(#) Nonlinearity			+-0.1 % of FS (Best fit straight line)
+ *  (#) Noise					0.56 (max 0.95) º/s, sigma
+ * 	(#) Digital Sensitivity		DS_GYR
+ * 	(#) Bandwidth				50 Hz
+ *
+ *	Accelerometer:
+ *
+ * 	(#) Dynamic Range 			+-2 g
+ * 	(#) Offset					+-30 mg
+ * 	(#) Nonlinearity			+-0.4 (max +-1.0) % of FS
+ *  (#) Noise					0.6 (max 0.8) mg, sigma
+ * 	(#) Digital Sensitivity		DS_ACC
+ * 	(#) Bandwidth				50 Hz
+ *
+ *	Magnetometer:
+ *
+ * 	(#) Dynamic Range 			+-1.9 gauss
+ * 	(#) Drift					2700 ppm/ºC
+ * 	(#) Nonlinearity			+-0.5 % of FS (Best fit straight line)
+ *  (#) Noise					0.00056 (max 0.0015) gauss, sigma
+ * 	(#) Digital Sensitivity		DS_MAG
+ * 	(#) Bandwidth				50 Hz
+ *
+ *  Thermometer:
+ *
+ * 	(#) Digital Sensitivity		DS_TMP
+ */
+
---
+++ src/imu_node.cpp
@@ -0,0 +1,199 @@
+// General
+#include <errno.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <time.h>
+#include <cmath>
+#include <termios.h>
+
+// ROS
+#include "ros/ros.h"
+#include "sensor_msgs/Imu.h"
+#include "sensor_msgs/MagneticField.h"
+#include <tf/transform_datatypes.h>
+
+// Serial Port Headers (serialcom-termios)
+#include "serialcom.h"
+
+// Project Headers
+#include "main.h"
+#include "imu.h"
+// pthread period function
+#define TASK_PERIOD_US  (SAMPLING_PERIOD*1000000)
+#define PI 3.14159265
+#define G 9.807 // m/s^2
+
+// int gps_ok = 0;
+int imu_ok = 0;
+
+// gps_t gps_values;
+imu_t imu_values;
+
+void setup_sig_handler();
+void sig_handler(int sig);
+
+int main(int argc, char** argv)
+{
+    int imu_new;
+    // Initial messages
+    // GPS_IMU("Outdoor robot localization system");
+    ros::init(argc, argv, "imu_node");
+    
+    ros::NodeHandle n;
+
+    ros::Publisher imu_pub = n.advertise<sensor_msgs::Imu>("imu/data_raw", 1000);
+    ros::Publisher imu_mag_pub = n.advertise<sensor_msgs::MagneticField>("imu/mag", 1000);
+
+    sensor_msgs::Imu data;
+    sensor_msgs::MagneticField mag;
+    ros::Rate loop_rate(50);
+    int seq = 0;
+
+    setup_sig_handler();
+
+    // Initialize GPS/IMU
+    if(!init_all())
+        return 1;
+    
+    // timer_start();
+    
+    // Print out running time and update MAT file every second
+    while(ros::ok())
+    {   
+        // usleep(1000000);
+        printf("IMU\n");
+        imu_new = imu_get_data(&imu_values);
+        imu_print_formatted(&imu_values);
+
+        // data.header.seq = seq;
+        data.header.stamp = ros::Time::now();
+        data.header.frame_id = "imu_frame";
+
+        // tf::Quaternion quat;
+        // quat.setRPY(imu_values.m[0], imu_values.m[1], imu_values.m[2]);
+
+        mag.header = data.header;
+        mag.magnetic_field.x = imu_values.m[0];
+        mag.magnetic_field.y = imu_values.m[1];
+        mag.magnetic_field.z = imu_values.m[2];
+        mag.magnetic_field_covariance[0] = 0.0015;
+        mag.magnetic_field_covariance[1] = 0;
+        mag.magnetic_field_covariance[2] = 0;
+        mag.magnetic_field_covariance[3] = 0;
+        mag.magnetic_field_covariance[4] = 0.0015;
+        mag.magnetic_field_covariance[5] = 0;
+        mag.magnetic_field_covariance[6] = 0;
+        mag.magnetic_field_covariance[7] = 0;
+        mag.magnetic_field_covariance[8] = 0.0015;
+
+        // tf::quaternionTFToMsg(quat, data.orientation);
+        data.orientation = geometry_msgs::Quaternion();
+        data.orientation_covariance[0] = -1;
+        data.orientation_covariance[1] = -1;
+        data.orientation_covariance[2] = -1;
+        data.orientation_covariance[3] = -1;
+        data.orientation_covariance[4] = -1;
+        data.orientation_covariance[5] = -1;
+        data.orientation_covariance[6] = -1;
+        data.orientation_covariance[7] = -1;
+        data.orientation_covariance[8] = -1;
+
+        data.angular_velocity.x = imu_values.g[0] * PI / 180.0;
+        data.angular_velocity.y = imu_values.g[1] * PI / 180.0;
+        data.angular_velocity.z = imu_values.g[2] * PI / 180.0;
+        data.angular_velocity_covariance[0] = 0.95;
+        data.angular_velocity_covariance[1] = 0;
+        data.angular_velocity_covariance[2] = 0;
+        data.angular_velocity_covariance[3] = 0;
+        data.angular_velocity_covariance[4] = 0.95;
+        data.angular_velocity_covariance[5] = 0;
+        data.angular_velocity_covariance[6] = 0;
+        data.angular_velocity_covariance[7] = 0;
+        data.angular_velocity_covariance[8] = 0.95;
+
+        data.linear_acceleration.x = imu_values.a[0] * G;
+        data.linear_acceleration.y = imu_values.a[1] * G;
+        data.linear_acceleration.z = -imu_values.a[2] * G;
+        data.linear_acceleration_covariance[0] = 0.8;
+        data.linear_acceleration_covariance[1] = 0;
+        data.linear_acceleration_covariance[2] = 0;
+        data.linear_acceleration_covariance[3] = 0;
+        data.linear_acceleration_covariance[4] = 0.8;
+        data.linear_acceleration_covariance[5] = 0;
+        data.linear_acceleration_covariance[6] = 0;
+        data.linear_acceleration_covariance[7] = 0;
+        data.linear_acceleration_covariance[8] = 0.8;
+
+        imu_mag_pub.publish(mag);
+        imu_pub.publish(data);
+
+        seq++;
+        loop_rate.sleep();
+        ros::spinOnce();
+    }
+    
+    // timer_stop();
+    
+    // Clean up and close GPS/IMU
+    if(!close_all())
+        return 1;
+        
+    // Close timing
+    // timing_close(timer_fp);
+    
+    return 0;
+}
+
+int init_all()
+{
+    // Initialize and open IMU serial port
+    if((imu_ok = imu_init("/dev/ttyUSB0")) != 1)
+        WARN("could not initialize IMU!");
+    else
+        INFO("IMU initialized...");
+    
+    // If nothing is working, fails
+    if(!imu_ok)
+        return 0;
+    
+    return 1;
+}
+
+int close_all()
+{
+    // Clean up and close IMU serial port
+    if(!imu_close())
+        WARN("Could not close IMU!");
+    else
+        INFO("IMU closed.");
+
+    GPS_IMU("Goodbye!");
+    
+    return 1;
+}
+
+void sig_handler(int sig)
+{
+    INFO("Interrupted! Closing...");
+    
+    // Close everything cleanly before exiting
+    if(!close_all())
+    {
+        FATAL("could not close cleanly!");
+        exit(1);
+    }
+    
+    exit(0);
+}
+
+void setup_sig_handler()
+{
+    signal(SIGINT, &sig_handler);
+    signal(SIGTSTP, &sig_handler);
+    signal(SIGABRT, &sig_handler);
+    signal(SIGTERM, &sig_handler);
+}
